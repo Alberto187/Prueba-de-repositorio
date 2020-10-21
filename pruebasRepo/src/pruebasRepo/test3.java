c58fffbd199db0d3af58e49f0aba34665f83d609
Alberto Mateo <mmateo@interware.com.mx>
2020-10-20 23:30:41 -0500

    Test 3

---
+++ pruebasRepo/src/pruebasRepo/test3.java
@@ -0,0 +1,12 @@
+package pruebasRepo;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		for(int a=0;a<10;a++) {
+			System.out.println(a);
+		}
+	}
+
+}
