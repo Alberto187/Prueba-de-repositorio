4138aa779721c21fdcde63e15b50e390302c52aa
Alberto Mateo <mmateo@interware.com.mx>
2020-10-20 23:33:11 -0500

    Modificar repor1

--- pruebasRepo/src/pruebasRepo/repo1.java
+++ pruebasRepo/src/pruebasRepo/repo1.java
@@ -3,7 +3,9 @@ package pruebasRepo;
 public class repo1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		System.out.println("Hola mundo");
+		for(int a=0;a<10;a++) {
+			System.out.println(a);
 	}
-
+	}
 }
